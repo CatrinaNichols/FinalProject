b2a00d3391995c5d64b46aa439bed5ad832bfe62
Ryan Neumont Laptop <Ryan Neumont Laptop@10.10.16.81>
2018-02-27 15:01:37 -0700

    Added map system and move method

--- controllers/Mechanics.java
+++ controllers/Mechanics.java
@@ -1,8 +1,17 @@
 package controllers;
 
+import java.util.ArrayList;
+
+import characterModels.BaseCharacter;
+import models.Item;
+
 public class Mechanics {
-ArrayList<Items> inventory = new ArrayList<>();
-Hero hero = new Hero;
+BaseCharacter[][] map;
+ArrayList<Item> inventory = new ArrayList<>();
+BaseCharacter hero = new BaseCharacter();
+//Coordinates for current location
+int x=0;
+int y=0;
 
 public void save(String filePath) {
 	//Character location
@@ -38,6 +47,48 @@ public void dropLoot() {
 	
 }
 
+public void move(String action) {
+	switch(action) {
+	case "up":
+		if(y-1 < 0) {
+			System.out.println("Can't move in that direction");
+		} else {
+			map[y][x] = null;
+			y--;
+			map[y][x] = hero;
+		}
+		break;
+	case "down":
+		if(y+1 >= map.length) {
+			System.out.println("Can't move in that direction");
+		} else {
+			map[y][x] = null;
+			y++;
+			map[y][x] = hero;
+		}
+		break;
+	case "left":
+		if(x-1 < 0) {
+			System.out.println("Can't move in that direction");
+		} else {
+			map[y][x] = null;
+			x--;
+			map[y][x] = hero;
+		}
+		break;
+	case "right":
+		if(x+1 >= map[y].length) {
+			System.out.println("Can't move in that direction");
+		} else {
+			map[y][x] = null;
+			x++;
+			map[y][x] = hero;
+		}
+		break;
+	}
+	
+}
+
 public void engine(String action) {
 	switch(action) {
 	case "save":
@@ -48,6 +99,9 @@ public void engine(String action) {
 		break;
 	case "inventory":
 		inventoryManagement();
+		break;
+	case "move":
+		move(action);
 		break;
 	}
 }
