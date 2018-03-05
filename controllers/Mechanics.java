ba51ff3b978a507154d9389c5c3781430734dbe5
Ryan Neumont Laptop <Ryan Neumont Laptop@10.10.16.88>
2018-03-05 13:54:57 -0700

    Updated mechanics

--- controllers/Mechanics.java
+++ controllers/Mechanics.java
@@ -1,19 +1,41 @@
 package controllers;
 
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 
 import characterModels.BaseCharacter;
-import models.Item;
+import characterModels.Hero;
+import characterModels.Monster;
+import enums.*;
+import itemModels.Item;
 
 public class Mechanics {
 	BaseCharacter[][] map;
 	ArrayList<Item> inventory = new ArrayList<>();
-	Hero hero = new Hero();
+	Hero hero;
 	// Coordinates for current location
 	int x = 0;
 	int y = 0;
+	
+	public void newGame() {
+		String name = "placeholder";
+		String proffession = "warrior";
+		switch(proffession) {
+		case "warrior":
+			hero = new Hero(name, HeroClass.WARRIOR);			
+			break;
+		case "ranger":
+			hero = new Hero(name, HeroClass.RANGER);			
+			break;
+		case "wizard":
+			hero = new Hero(name, HeroClass.WIZARD);			
+			break;
+		}
+		
+	}
 
 	public void save() {
 		String filePath = "saves/saveFile.ser";
@@ -22,9 +44,24 @@ public class Mechanics {
 		//Story progression
 		
 
-		FileOutputStream fos = new FileOutputStream(filePath);
-		ObjectOutputStream oos = new ObjectOutputStream(fos);
-		oos.writeObject(map, inventory, hero, x ,y);
+		FileOutputStream fos;
+		ObjectOutputStream oos;
+		try {
+			fos = new FileOutputStream(filePath);
+			oos = new ObjectOutputStream(fos);
+			oos.writeObject(map);
+			oos.writeObject(hero);
+			oos.writeObject(inventory);
+			oos.writeObject(x);
+			oos.writeObject(y);
+			
+			oos.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
 
 	}
 
@@ -39,70 +76,73 @@ public void combat() {
 	boolean battle = true;
 	boolean heroTurn = false;
 	
-	if(inPlains) {
-		PlainMonster monster = new PlainMonster();
-	} else if (inCave) {
-		CaveMonster monster = new CaveMonster();
-	} else if (inCastle) {
-		CastleMonster monster = new CastleMonster();
-	}
+	Monster monster = new Monster(MonsterTypes.PLAINS_GOBLIN_S, 1);
 	
-	if(hero.getDexMod() > monster.getDexMod()) {
-		heroTurn = true;
-	}
+//	if(inPlains) {
+//		PlainMonster monster = new PlainMonster();
+//	} else if (inCave) {
+//		CaveMonster monster = new CaveMonster();
+//	} else if (inCastle) {
+//		CastleMonster monster = new CastleMonster();
+//	}
+	
+//	if((hero.getDexMod() > monster.getDexMod()) {
+//		heroTurn = true;
+//	}
 	
 	do {
 		if(heroTurn) {
-			takeTurn(hero, monster);
-			!heroTurn;
+			takeTurn(hero, monster, heroTurn);
+			heroTurn = !heroTurn;
 		} else {
-			takeTurn(monster, hero);
-			!heroTurn;
+			takeTurn(monster, hero, heroTurn);
+			heroTurn = !heroTurn;
 		}
 		
-		if(hero.getCurrentHP() == 0) {
-			hero dies
+		if(!hero.getIsAlive()) {
+			//game over
 			battle = false;
-		} else if (monster.getCurrentHP() == 0) {
-			monster dies
+		} else if(!monster.getIsAlive()) {
+			
 			battle = false;
 		}
+		
 	} while (!battle);
 }
 
-	public void takeTurn(BaseCharacter attacker, BaseCharacter defender) {
-		if (attacker.getClass() == Hero) {
+	public void takeTurn(BaseCharacter attacker, BaseCharacter defender, boolean heroTurn) {
+		if (heroTurn) {
 			// add in menu for hero's turn
 		} else {
-			int damage = attacker.attack(defender.getArmor());
+			int damage = attacker.getDamage();
 			defender.takeDamage(damage);
 		}
 	}
 
-public void inventoryManagement() {
-	//Give player option to discard loot
-	//show all loot available
-	//Equipment of items
-	
-	
-	take in choice of whether to discard, equip, or exit
-	
-	if(discard) {
-		inventory.remove();
-	}else if(use) {
-		if(item.instanceOf(Armor)) {
-			hero.setEquippedArmor(item);
-		}else if(item.instanceOf(Weapon)) {
-			hero.setEquippedWeapon(item);
-		}else if(item.instanceOf(HealingItem)) {
-			item.use(hero);
-		}else {
-		 not available to use
-	}
-	}else if(exit) {
-		back to menu
-	}
-}
+//public void inventoryManagement() {
+//	//Give player option to discard loot
+//	//show all loot available
+//	//Equipment of items
+//	
+//	
+//	take in choice of whether to discard, equip, or exit
+//	
+//	if(discard) {
+//		inventory.remove();
+//	}else if(use) {
+//		if(item.instanceOf(Armor)) {
+//			hero.setEquippedArmor(item);
+//		}else if(item.instanceOf(Weapon)) {
+//			hero.setEquippedWeapon(item);
+//		}else if(item.instanceOf(HealingItem)) {
+//			item.use(hero);
+//		}else {
+//		 not available to use
+//	}
+//	}else if(exit) {
+//		back to menu
+//	}
+//}
 
 	public void dropLoot() {
 		// Determine what to drop based on monster
@@ -155,16 +195,19 @@ public void inventoryManagement() {
 	public void engine(String action) {
 		switch (action) {
 		case "save":
-			save();
+			//save();
 			break;
 		case "load":
-			load();
+			//load();
 			break;
 		case "inventory":
-			inventoryManagement();
+			//inventoryManagement();
 			break;
 		case "move":
 			move(action);
+			break;
+		case "newGame":
+			newGame();
 			break;
 		}
 	}
